c933ec183fa3a1eefaa1c8323e920de095c26663
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-07 15:14:53 +0300

    ok

---
+++ User.cs
@@ -0,0 +1,10 @@
+public abstract class User{
+    public string firstname{ get; set;}
+    public string middlename {get;set;}
+    public string lastname{get;set;}
+
+    public int userid{get;}
+    public DateTime dateofbirth{get;set;}
+    public char gender{get;set;}
+
+}
