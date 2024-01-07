6720c0ab380f4ac5441e00b9ff781d2299b4095d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-07 15:21:46 +0300

    ok

--- User.cs
+++ User.cs
@@ -1,5 +1,5 @@
 public abstract class User{
-
+    //use of "?" in the string is to tell the c# that first,middle and last are empty if not declared  
     protected string? FirstName{ get; set;}
     protected string? MiddleName {get;set;}
     protected string? LastName{get;set;}
