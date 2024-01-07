a05ade4be07370c72f0a8e857a6c876d278efd2d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-07 15:33:18 +0300

    ok

--- Courese.cs
+++ Courese.cs
@@ -1,4 +1,7 @@
 public class Courese{
+    public Courese(){
+        
+    }
     protected string? Name{get;set;}
     protected int Id{get;}
     public int CreaditHour{get; private set;}
--- Instructor.cs
+++ Instructor.cs
@@ -1,5 +1,6 @@
 public class Instructor:User{
     string password=string.Empty;
+    int creadithour=new Courese().CreaditHour;
     public string Password{
         get{
             return this.password;
@@ -7,5 +8,9 @@ public class Instructor:User{
         set{
             this.password=value;
         }
+    }
+    public int CreaditHour{get{
+        return this.creadithour;
+    }
     }
 }
