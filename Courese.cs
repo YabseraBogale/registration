9dc7cce8135cd3ff09eb74a537c2594bca884cf3
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-07 15:20:31 +0300

    ok

---
+++ Courese.cs
@@ -0,0 +1,3 @@
+public class Courese{
+    protected string Name{get;set;}
+}
--- User.cs
+++ User.cs
@@ -1,8 +1,8 @@
 public abstract class User{
-    protected string FirstName{ get; set;}
-    protected string MiddleName {get;set;}
-    protected string LastName{get;set;}
 
+    protected string? FirstName{ get; set;}
+    protected string? MiddleName {get;set;}
+    protected string? LastName{get;set;}
     protected int UserId{get;}
     protected DateTime DateOfBirth{get;set;}
     protected char Gender{get;set;}
