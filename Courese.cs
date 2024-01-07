739bdc46a9898c1e6ec26a54edcf3f18d59cd935
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-07 15:25:39 +0300

    ok

--- Courese.cs
+++ Courese.cs
@@ -1,3 +1,5 @@
 public class Courese{
-    protected string Name{get;set;}
+    protected string? Name{get;set;}
+    protected int Id{get;}
+    public int CreaditHour{get; private set;}
 }
--- User.cs
+++ User.cs
@@ -1,8 +1,10 @@
 public abstract class User{
     //use of "?" in the string is to tell the c# that first,middle and last are empty if not declared  
+    //class could be defined as follows if null should be allowed for the name
     protected string? FirstName{ get; set;}
     protected string? MiddleName {get;set;}
     protected string? LastName{get;set;}
+    // no use of "?" in int type b/c of int can know ahead of time as 0
     protected int UserId{get;}
     protected DateTime DateOfBirth{get;set;}
     protected char Gender{get;set;}
